87d2428848e30bbe1533c08ee310d6f7c566b6d4
Simon Pratt <simon@pr4tt.com>
2013-05-19 14:23:25 -0400

    Pulled out demo code from radius.js

--- radius.js
+++ radius.js
@@ -1,14 +1,9 @@
 var radius = (function(radius, undefined) {
 	var map;
 
-	radius.range_of_a_cf18 = 3330000;
-	radius.ottawa = new google.maps.LatLng(45.417,-75.683);
-	radius.cold_lake = new google.maps.LatLng(54.45,-110.167);
-	radius.bagotville = new google.maps.LatLng(48.317,-79.983);
-	
-	radius.init = function radius_init() {
+	radius.init = function radius_init(centre) {
 		var mapOptions = {
-			center: radius.ottawa,
+			center: centre,
 			zoom: 3,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
@@ -32,22 +27,3 @@ var radius = (function(radius, undefined) {
 
 	return radius;
 })(radius || {});
-google.maps.event.addDomListener(window, 'load', radius.init);
-google.maps.event.addDomListener(window, 'load', function() {
-	var cold_lake_range = radius.drawCircle(radius.cold_lake,
-											radius.range_of_a_cf18,
-											'#ff0000',
-											'#ff0000');
-	var cold_lake_range = radius.drawCircle(radius.cold_lake,
-											radius.range_of_a_cf18/2,
-											'#ff0000',
-											'#ff0000');
-	var bagotville_range = radius.drawCircle(radius.bagotville,
-											 radius.range_of_a_cf18,
-											 '#0000ff',
-											 '#0000ff');
-	var bagotville_range = radius.drawCircle(radius.bagotville,
-											 radius.range_of_a_cf18/2,
-											 '#0000ff',
-											 '#0000ff');
-});
