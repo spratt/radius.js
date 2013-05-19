8d7f72eb403634aee17d192fd125d450abbfb7f1
Simon Pratt <simon@pr4tt.com>
2013-05-19 13:58:44 -0400

    Now centred on Ottawa

--- radius.js
+++ radius.js
@@ -1,13 +1,17 @@
 var radius = (function(radius, undefined) {
 	radius.init = function radius_init() {
 		var mapOptions = {
-			center: new google.maps.LatLng(-34.397, 150.644),
-			zoom: 8,
+			center: new google.maps.LatLng(45.417,-75.683),
+			zoom: 4,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
 		var map = new google.maps.Map(document.getElementById("map-canvas"),
 									  mapOptions);
 	}
+
+	radius.drawCircle = function radius_drawCircle(centre, radius) {
+		
+	};
 
 	return radius;
 })(radius || {});
