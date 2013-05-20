633a2a382654d22c9964fb7492d3d7661136d05c
Simon Pratt <simon@pr4tt.com>
2013-05-20 08:14:55 -0400

    Re-added 2d demo

--- cf18demo.js
+++ cf18demo.js
@@ -1,23 +1,17 @@
-(function(undefined) {
-	var mapID = 'map3d';
-	var ottawa = new radius.Coord(45.417,-75.683);
-	var range_of_a_cf18 = 3330;
-	var cold_lake = new radius.Coord(54.405,-110.279444);
-	var bagotville = new radius.Coord(48.330556,-70.996944);
-	
-	function noop() {}
-	function drawRanges() {
-		var cold_lake_range = radius.drawCircle('CFB Cold Lake',
-												cold_lake,
-												range_of_a_cf18 / 2,
-												'99ffff00',
-												'60ffff00');
-		var bagotville_range = radius.drawCircle('CFB Bagotville',
-												 bagotville,
-												 range_of_a_cf18 / 2,
-												 '9900ffff',
-												 '6000ffff');
-	}
-
-	radius.init('map3d',ottawa,drawRanges,noop);
-})();
+google.maps.event.addDomListener(window, 'load', function() {
+	var ottawa = new google.maps.LatLng(45.417,-75.683);
+	radius.init('map3d',ottawa);
+});
+google.maps.event.addDomListener(window, 'load', function() {
+	var range_of_a_cf18 = 3330000;
+	var cold_lake = new google.maps.LatLng(54.40,-110.267);
+	var bagotville = new google.maps.LatLng(48.317,-79.983);
+	var cold_lake_range = radius.drawCircle(cold_lake,
+											range_of_a_cf18/2,
+											'#ff0000',
+											'#ff0000');
+	var bagotville_range = radius.drawCircle(bagotville,
+											 range_of_a_cf18/2,
+											 '#0000ff',
+											 '#0000ff');
+});
---
+++ cf18demo3d.js
@@ -0,0 +1,23 @@
+(function(undefined) {
+	var mapID = 'map3d';
+	var ottawa = new radius.Coord(45.417,-75.683);
+	var range_of_a_cf18 = 3330;
+	var cold_lake = new radius.Coord(54.405,-110.279444);
+	var bagotville = new radius.Coord(48.330556,-70.996944);
+	
+	function noop() {}
+	function drawRanges() {
+		var cold_lake_range = radius.drawCircle3d('CFB Cold Lake',
+												  cold_lake,
+												  range_of_a_cf18 / 2,
+												  '99ffff00',
+												  '60ffff00');
+		var bagotville_range = radius.drawCircle3d('CFB Bagotville',
+												   bagotville,
+												   range_of_a_cf18 / 2,
+												   '9900ffff',
+												   '6000ffff');
+	}
+
+	radius.init3d('map3d',ottawa,drawRanges,noop);
+})();
